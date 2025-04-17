ff28e689948f3f24aa3dd578a0d59ccb593d34fb
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-04-17 12:00:42 -0300

    se agrega readme

---
+++ src/test/java/com/fin_app/finApp/FinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin_app.finApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
